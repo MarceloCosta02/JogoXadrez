8ffb09af1557586b2fc92cde977b1fa081b3284f
Ponte <marcelo.ponte@capgemini.com>
2020-07-08 12:56:41 -0300

    Implementando a logica de xequemate

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -44,6 +44,8 @@ namespace XadrezConsole
                         Console.ReadLine();
                     }
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
             }
             catch (TabuleiroException e)
             {
